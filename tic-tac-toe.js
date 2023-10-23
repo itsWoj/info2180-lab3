f69c70e09ab809a302e5baf6d3ecac619bf262bb
ItsWoj <itzparkes@gmail.com>
2023-10-23 18:08:54 -0500

    Added X Click

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,15 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() 
-{
+document.addEventListener("DOMContentLoaded", function() {
     const squares = document.querySelectorAll("#board > div");
     const statusMessage = document.getElementById("status");
     const newGameButton = document.querySelector(".btn");
 
     squares.forEach(square => {
         square.classList.add("square");
-    })
-
-
+    });
 
     
-}); 
-    
+    let currentPlayer = "X";
+    let gameBoard = ["", "", "", "", "", "", "", "", ""];
+    let gameActive = true;
+
+    function handleCellClick(index) {
+        if (gameBoard[index] === "" && gameActive) {
+            gameBoard[index] = currentPlayer;
+            squares[index].classList.add(currentPlayer);
+            squares[index].innerText = currentPlayer;
+        }
+    }
+
+    squares.forEach((square, index) => {
+        square.addEventListener("click", () => handleCellClick(index));
+        square.addEventListener("mouseover", () => handleCellHover(index));
+        square.addEventListener("mouseleave", () => handleCellLeave(index));
+    });
+
+});
+
